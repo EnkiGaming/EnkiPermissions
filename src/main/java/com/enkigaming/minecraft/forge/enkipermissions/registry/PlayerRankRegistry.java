e0ce987ff1716b61123fe2431ebe1018dc849843
HaniiPuppy <hanii.puppy@googlemail.com>
2014-10-13 14:04:46 +0100

    Implemented file handling for PlayerRankRegistry

--- src/main/java/com/enkigaming/minecraft/forge/enkipermissions/registry/PlayerRankRegistry.java
+++ src/main/java/com/enkigaming/minecraft/forge/enkipermissions/registry/PlayerRankRegistry.java
@@ -1,13 +1,20 @@
 package com.enkigaming.minecraft.forge.enkipermissions.registry;
 
+import com.enkigaming.minecraft.forge.enkilib.EnkiLib;
+import com.enkigaming.minecraft.forge.enkilib.filehandling.CSVFileHandler;
+import com.enkigaming.minecraft.forge.enkilib.filehandling.CSVFileHandler.CSVRowMember;
 import com.enkigaming.minecraft.forge.enkipermissions.ranks.Rank;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class PlayerRankRegistry
@@ -18,6 +25,79 @@ public class PlayerRankRegistry
     protected final File saveFolder;
     protected final Map<UUID, String> playerRanks = new HashMap<UUID, String>(); // Map<PlayerId, RankName>
     protected String defaultRank; // synchronize with ranks.
+    
+    protected Lock playerRanksLock = new ReentrantLock();
+    
+    protected CSVFileHandler makeFileHandler(File saveFolder)
+    {
+        return new CSVFileHandler("PlayerRankRegistry", saveFolder, "Not all rows could be read from playerranks.csv - as many as could be were.")
+        {
+            List<Map.Entry<UUID, String>> ranksList;
+            
+            @Override
+            protected void onNoFileToInterpret()
+            {}
+
+            @Override
+            protected List<String> getColumnNames()
+            { return Arrays.asList("Player ID", "Last recorded player name", "Rank"); }
+
+            @Override
+            protected void preInterpretation()
+            { playerRanksLock.lock(); }
+
+            @Override
+            protected boolean interpretRow(List<String> list)
+            {
+                if(list.size() != 3)
+                    return false;
+                
+                UUID playerId;
+                
+                try
+                { playerId = UUID.fromString(list.get(0)); }
+                catch(IllegalArgumentException e)
+                {
+                    System.out.println("Could not interpret row for " + list.get(1));
+                    return false;
+                }
+                
+                String rank = list.get(2);
+                
+                playerRanks.put(playerId, rank);
+                return true;
+            }
+
+            @Override
+            protected void postInterpretation()
+            { playerRanksLock.unlock(); }
+
+            @Override
+            protected void preSave()
+            {
+                playerRanksLock.lock();
+                
+                ranksList = new ArrayList<Map.Entry<UUID, String>>(playerRanks.entrySet());
+            }
+
+            @Override
+            protected List<CSVRowMember> getRow(int i)
+            {
+                String lastRecordedUsername = EnkiLib.getInstance().getUsernameCache().getLastRecordedNameOf(ranksList.get(i).getKey());
+                
+                if(lastRecordedUsername == null)
+                    lastRecordedUsername = "";
+                
+                return Arrays.asList(new CSVRowMember[]{new CSVRowMember(ranksList.get(i).getKey().toString(), false),
+                                                        new CSVRowMember(lastRecordedUsername,                 !lastRecordedUsername.isEmpty()),
+                                                        new CSVRowMember(ranksList.get(i).getValue(),          true)});
+            }
+
+            @Override
+            protected void postSave()
+            { playerRanksLock.unlock(); }
+        };
+    }
     
     public Map<UUID, String> getPlayerRanks()
     {
@@ -84,10 +164,4 @@ public class PlayerRankRegistry
     
     public String setDefaultRank(Rank rank)
     { return setDefaultRank(rank.getName()); }
-    
-    public void save()
-    {}
-    
-    public void load()
-    {}
 }
